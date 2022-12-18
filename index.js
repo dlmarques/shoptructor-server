0cc04546db8dceceea033def8e553b3132bf4abd
dlmarques <daniel.marquesedigital@gmail.com>
2022-12-18 19:21:36 +0000

    core

--- index.js
+++ index.js
@@ -4,15 +4,27 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
-dotenv.config();
+//variables
+const port = process.env.PORT  || 3001;
 
+//server config
+dotenv.config();
 app.use(cors());
 
+//route variables
+const authRoute = require("./routes/auth")
+
+
+//routes configuration
+app.use("/api/auth", authRoute)
+
+//db connect
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology: true}, async () => console.log("connected to DB"));
 
+//middleware
 app.use(express.json());
 
-const port = process.env.PORT  || 3001;
+
 
 app.listen(port, () => console.log(`server running on ${port}`))
---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+
+const router = require("express").Router();
+//const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+//const verifyToken = require("../utils/verifyToken");
+//const { registerValidation, loginValidation } = require("../utils/validation");
+
+router.get("/test", (req, res) => {
+  res.send(true);
+});
+
+module.exports = router;
