4c52eddbb78359478392567e76846dae2ceaa6d0
dlmarques <daniel.marquesedigital@gmail.com>
2022-12-24 18:41:32 +0000

    create register flow

--- models/User.js
+++ models/User.js
@@ -18,7 +18,19 @@ const userSchema = new mongoose.Schema({
     min: 8,
     validate:
       /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,1024}$/,
-  },
+    },
+    projectName: {
+        type: String,
+        required: true,
+        max: 255,
+        min: 6,
+    },
+    country: {
+        type: String,
+        required: true,
+        max: 255,
+        min: 6,
+    },
 });
 
 module.exports = mongoose.model("User", userSchema);
