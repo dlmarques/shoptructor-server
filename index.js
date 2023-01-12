823e60070602b6a418a06c8dfb13f8df1157037b
dlmarques <daniel.marquesedigital@gmail.com>
2023-01-12 00:45:09 +0000

    fix db connection

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const bp = require('body-parser')
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bp = require("body-parser");
 
 //variables
-const port = process.env.PORT  || 3001;
+const port = process.env.PORT || 3001;
 
 //server config
 dotenv.config();
 app.use(cors());
-app.use(bp.json())
-app.use(bp.urlencoded({extended: true}))
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 
 //route variables
-const authRoute = require("./routes/auth")
-
+const authRoute = require("./routes/auth");
 
 //routes configuration
-app.use("/api/auth", authRoute)
+app.use("/api/auth", authRoute);
 
 //db connect
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology: true}, async () => console.log("connected to DB"));
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DB_CONNECT, async () =>
+  console.log("connected to DB")
+);
 
 //middleware
 app.use(express.json());
 
-
-
-app.listen(port, () => console.log(`server running on ${port}`))
+app.listen(port, () => console.log(`server running on ${port}`));
